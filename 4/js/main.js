a8afb5edf8ae59e76f17bd705f5ce6c5bc7c82f1
Keks <keks@htmlacademy.ru>
2023-03-28 15:14:41 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,8 @@
+import { generateData } from './data.js';
+
+// генерация массива из 25 объектов
+const dataArray = generateData();
+
+// вывод данных в консоль
+// eslint-disable-next-line no-console
+console.log(dataArray);
